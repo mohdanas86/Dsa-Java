ee79b4d434bd0a0d47bdcd6470fc6970a6718c2d
mohdanas86 <anasalam7739@gmail.com>
2026-02-16 16:40:13 +0530

    Reintroduce LargestElementOfArray and SecondLargestNumber classes for finding largest and second largest elements in an array

--- src/Array/EASY/LargestElementOfArray.java
+++ src/Array/EASY/LargestElementOfArray.java
@@ -1,4 +1,4 @@
-package Array;
+package Array.EASY;
 
 import java.util.Arrays;
 import java.util.Vector;
--- src/Array/EASY/SecondLargestNumber.java
+++ src/Array/EASY/SecondLargestNumber.java
@@ -1,4 +1,4 @@
-package Array;
+package Array.EASY;
 
 public class SecondLargestNumber {
 
